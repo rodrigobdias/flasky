a8212494738cad0828f38f2b494ed73a4eb75e09
Rodrigo <rodrigobdias@gmail.com>
2019-11-13 15:14:27 -0300

    Integration with the Python Shell

--- hello.py
+++ hello.py
@@ -77,3 +77,6 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500
 
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Role=Role)
