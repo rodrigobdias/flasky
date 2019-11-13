5cf97889eee6704fcb4e171f057b5a4b4858e160
Rodrigo <rodrigobdias@gmail.com>
2019-11-13 15:24:07 -0300

    adding support for database migrations with the 'init' subcommand

--- hello.py
+++ hello.py
@@ -8,6 +8,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -19,6 +20,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
 
 class Role(db.Model):
     __tablename__ = 'roles'
