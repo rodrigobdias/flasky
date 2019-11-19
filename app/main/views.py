8e0ca9731946db9f9598006e8fc77d02e13a427b
Rodrigo <rodrigobdias@gmail.com>
2019-11-19 15:57:10 -0300

    url_for('index') -> url_for('.index')

--- app/main/views.py
+++ app/main/views.py
@@ -21,7 +21,7 @@ def index():
         else:
             session['known'] = True
         session['name'] = form.name.data
-        return redirect(url_for('index'))
+        return redirect(url_for('.index'))
 
     return render_template('index.html',
                             form=form,
