0ce9df62bb80ca31efae5164128243cfc741a771
Rodrigo <rodrigobdias@gmail.com>
2020-01-30 14:51:06 -0300

    update

--- hello.py
+++ hello.py
@@ -101,8 +101,8 @@ def index():
         session['name'] = form.name.data
         return redirect(url_for('index'))
 
-    return render_template('index.html', 
-                            form=form, 
+    return render_template('index.html',
+                            form=form,
                             name=session.get('name'),
                             known=session.get('known', False),
                             current_time=datetime.utcnow())
