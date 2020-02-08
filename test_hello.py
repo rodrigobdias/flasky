b279f0a7394691bcf45c9f81fae032ee01cd871e
Rodrigo <rodrigobdias@gmail.com>
2020-02-07 22:42:52 -0300

    redirect test (code 302)

--- test_hello.py
+++ test_hello.py
@@ -9,17 +9,9 @@ class TestHello(unittest.TestCase):
     """Testes para hello.py"""
 
     def setUp(self):
-        app = app.test_client()
-        self.response = app.get('/')
+        tdd = app.test_client()
+        self.response = tdd.get('/debian')
 
-   # Testamos se a resposta e 200 ("ok")
+   # Testamos se a resposta e 302 ("redirecionamento")
     def test_get(self):
-        self.assertEqual(200, self.response.status_code)
-
-    # Testamos se a nossa home retorna a string "Test TDD"
-    def test_html_string_response(self):
-        self.assertEqual("Test TDD", self.response.data.decode('utf-8'))
-
-    # Testamos se o content_type da resposta da home esta correto
-    def test_content_type(self):
-        self.assertIn('text/html', self.response.content_type)        
+        self.assertEqual(302, self.response.status_code)
