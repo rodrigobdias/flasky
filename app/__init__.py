c6725ba85753f64a4f25611152ee4cd2dd1894e8
Rodrigo <rodrigobdias@gmail.com>
2020-03-04 10:19:45 -0300

    Using an Application Factory

--- app/__init__.py
+++ app/__init__.py
@@ -13,7 +13,6 @@ moment = Moment()
 
 db = SQLAlchemy()
 
-
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
@@ -24,7 +23,7 @@ def create_app(config_name):
     moment.init_app(app)
     db.init_app(app)
 
-    from .main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
+#   from .main import main as main_blueprint
+#   app.register_blueprint(main_blueprint)
     
     return app
