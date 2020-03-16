7addf7c57fa059e8458c52e58b5272e77a7cad52
Rodrigo <rodrigobdias@gmail.com>
2020-03-16 16:22:00 -0300

    updates to User model to support user logins

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from . import db
+from flask_login import UserMixin
 
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -12,9 +13,10 @@ class Role(db.Model):
         return '<Role %r>' % self.name
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
